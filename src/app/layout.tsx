2232700f4e361989a7487c2c03b03849d00c1b81
priest-2105 <priest2105@gmail.com>
2026-08-07 22:40:28 +0100

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import favicon from 'public/brand/blick-favicon.svg'
 
 const inter = Inter({
   subsets: ["latin"],
